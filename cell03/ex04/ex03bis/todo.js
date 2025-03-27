31e30cd6343ba433ee23848e97448e363cbf81f6
pchangno <67160019@kmitl.ac.th>
2025-03-27 18:29:31 +0700

    modified:   cell03/ex04/ex02bis/calc.css 	modified:   cell03/ex04/ex03bis/index.html 	modified:   cell03/ex04/ex03bis/todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,3 +1,46 @@
-setInterval(function () {
-    alert("Please Enter TO DO LIST :");
-  }, 30000);
+$(document).ready(function() {
+  const $ft_list = $("#ft_list");
+  const name = "mycookie";
+ 
+  function createTodo() {
+      const txt = prompt("Please Enter TO DO LIST :");      
+      if (txt) {
+          const cookie = Cookies.get(name);
+          let values;
+          if(cookie) {
+              values = JSON.parse(cookie);
+          } else {
+              values = [];
+          }
+          values.push(txt);
+          Cookies.set(name, JSON.stringify(values));
+          const $node = $("<div>").text(txt).click(function() {
+              if (confirm("Do you want to Delete?")) {
+                  $(this).remove();
+                  Cookies.remove(name);
+                  
+              }
+          });
+          $ft_list.prepend($node);
+      }
+  }
+
+  if (Cookies.get(name)) {
+      const cookie = Cookies.get(name);
+      values = JSON.parse(cookie)
+      values.forEach(function(val) {
+          if (val) {
+              const $node = $("<div>").text(val).click(function() {
+                  if (confirm("Do you want to Delete?")) {
+                      $(this).remove();
+                      Cookies.remove(val);
+                      
+                  }
+              });
+              $ft_list.prepend($node);
+          }
+      });
+  }
+
+  $("#create").click(createTodo);
+});
